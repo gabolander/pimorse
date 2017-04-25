4b8bc36e2ed9d89e02b5cf81ca7c5a24dd4e191f
Gabriele Zappi <gabriele.zappi@gmail.com>
2017-04-25 15:35:35 +0200

    add first release (#2) of python version, but NOT YET WORKING as expected

--- pimorse.py
+++ pimorse.py
@@ -9,6 +9,42 @@ BeepPin = 11    # pin11
 
 
 pins = [11, 12, 13, 15, 16, 18, 22, 7]
+default_msg = "Hello, world!"
+
+Kalfav = [ 'A','B','C','D','E','F','G','H','I','J',
+            'K','L','M','N','O','P','Q','R','S','T',
+            'U','V','W','X','Y','Z', ## Alphabet chars
+            '1','2','3','4','5','6','7','8','9','0',' ', ## Numbers + space
+            '.',',',':','?','=','-','(',')','"','\'','/','_','@','!' ## Symbols
+        ]
+
+Kmorsev = [
+    ".-","-...","-.-.","-..",".","..-.","--.","....","..",".---",
+    "-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-",
+    "..-","...-",".-- ","-..-","-.--","--..", # Alphabet chars
+    ".----","..---","...--","....-",".....",
+    "-....","--...","---..","----.","-----"," ", # Numbers + space
+    ".-.-.-","--..--","---...","..--..","-...-","-....-","-.--.",
+    "-.--.-",".-..-.",".----.","-..-.","..--.-",".--.-.","-.-.--" # Symbols
+    ]
+
+def soundmorse(seq):
+    return 1
+
+def morse_decode(letter):
+    Index=0
+    maxindex=len(Kalfav)
+    for kalfa in Kalfav:
+        if kalfa == letter:
+            break
+        else:
+            Index=Index+1
+    if Index>=maxindex:
+        return ""
+    else:
+        return Kmorsev[Index]
+
+
 
 def setup():
         GPIO.setwarnings(False)
@@ -18,14 +54,30 @@ def setup():
 
 def loop():
 	while True:
-		GPIO.output(BeepPin, GPIO.LOW)
-		time.sleep(0.1)
-		GPIO.output(BeepPin, GPIO.HIGH)
-		time.sleep(0.1)
+#		GPIO.output(BeepPin, GPIO.LOW)
+#		time.sleep(0.1)
+#		GPIO.output(BeepPin, GPIO.HIGH)
+#		time.sleep(0.1)
 # 		for pin in pins:
 # 			GPIO.output(pin, GPIO.LOW)	
 # 			time.sleep(0.5)
 # 			GPIO.output(pin, GPIO.HIGH)
+            message = raw_input("Enter a message to transmit (\""+default_msg+"\" default message, \"STOP\" to exit) : " )
+            if message == "STOP":
+                break
+
+            if message == "":
+                message = default_msg
+
+            # print "message: ", message
+            Index=0
+            while Index<len(message):
+                letter=message.upper()[Index]
+                morseseq=morse_decode(letter)
+                print "letter: ", letter, " morse seq: ", morseseq
+                soundmorse(morseseq)
+                Index=Index+1
+            print '\n'
 
 def destroy():
 	GPIO.output(BeepPin, GPIO.HIGH)    # beep off
@@ -36,6 +88,7 @@ if __name__ == '__main__':     # Program start from here
 	setup()
 	try:
 		loop()
+                destroy()
 	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
 		destroy()
 
