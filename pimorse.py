9445a96cfcb9e6e5baf2920b828919ab73200435
Gabriele Zappi <gabriele.zappi@gmail.com>
2017-04-26 10:34:45 +0200

    First (maybe) working python version. (Still to test)

--- pimorse.py
+++ pimorse.py
@@ -7,8 +7,19 @@ BeepPin = 11    # pin11
 # MotorPin2   = 12    # pin12
 # MotorEnable = 13    # pin13
 
+# I define some vars that I treat as constants ...
 
-pins = [11, 12, 13, 15, 16, 18, 22, 7]
+DOT_MS_LEN=0.08     # "BASE" DOT LENGTH: This is the main 'dot' length which all
+                    # the other delay/durations are based on.
+                    # Decrease it, to make it playing faster.
+                    # Increase it, to make it slower.
+                    # Don't touch other DASH_* DELAY_* values instead, because
+                    # they are multipliers of DOT_MS_LEN.
+DASH_MS_LEN=(DOT_MS_LEN*3)
+DELAY_MS_DOTS=(DOT_MS_LEN*1)
+DELAY_MS_LETTERS=(DOT_MS_LEN*3)
+DELAY_MS_WORDS=(DOT_MS_LEN*5)
+
 default_msg = "Hello, world!"
 
 Kalfav = [ 'A','B','C','D','E','F','G','H','I','J',
@@ -29,6 +40,20 @@ Kmorsev = [
     ]
 
 def soundmorse(seq):
+    Index=0
+    while Index<len(seq):
+        sign=seq[Index]
+        GPIO.output(BeepPin, GPIO.LOW)
+        if sign=='-':
+            time.sleep(DASH_MS_LEN)
+        else:
+            time.sleep(DOT_MS_LEN)
+
+        GPIO.output(BeepPin, GPIO.HIGH)
+        time.sleep(DELAY_MS_DOTS)
+
+    # Delay at end of letter
+    time.sleep(DELAY_MS_LETTERS-DELAY_MS_DOTS)
     return 1
 
 def morse_decode(letter):
@@ -47,21 +72,13 @@ def morse_decode(letter):
 
 
 def setup():
-        GPIO.setwarnings(False)
-	GPIO.setmode(GPIO.BOARD)        # Numbers GPIOs by physical location
-	GPIO.setup(BeepPin, GPIO.OUT)   # Set BeepPin's mode is output
-	GPIO.output(BeepPin, GPIO.HIGH) # Set BeepPin high(+3.3V) to off beep
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)        # Numbers GPIOs by physical location
+    GPIO.setup(BeepPin, GPIO.OUT)   # Set BeepPin's mode is output
+    GPIO.output(BeepPin, GPIO.HIGH) # Set BeepPin high(+3.3V) to off beep
 
 def loop():
-	while True:
-#		GPIO.output(BeepPin, GPIO.LOW)
-#		time.sleep(0.1)
-#		GPIO.output(BeepPin, GPIO.HIGH)
-#		time.sleep(0.1)
-# 		for pin in pins:
-# 			GPIO.output(pin, GPIO.LOW)	
-# 			time.sleep(0.5)
-# 			GPIO.output(pin, GPIO.HIGH)
+    while True:
             message = raw_input("Enter a message to transmit (\""+default_msg+"\" default message, \"STOP\" to exit) : " )
             if message == "STOP":
                 break
@@ -74,21 +91,23 @@ def loop():
             while Index<len(message):
                 letter=message.upper()[Index]
                 morseseq=morse_decode(letter)
-                print "letter: ", letter, " morse seq: ", morseseq
+                print "letter: ", letter, " morse seq: ", morseseq # GABODebug
                 soundmorse(morseseq)
                 Index=Index+1
             print '\n'
 
 def destroy():
-	GPIO.output(BeepPin, GPIO.HIGH)    # beep off
-	GPIO.cleanup()                     # Release resource
+    GPIO.output(BeepPin, GPIO.HIGH)    # beep off
+    GPIO.cleanup()                     # Release resource
 
 if __name__ == '__main__':     # Program start from here
-        print 'Press Ctrl+C to end the program...'
-	setup()
-	try:
-		loop()
-                destroy()
-	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
-		destroy()
+    print 'Press Ctrl+C to end the program...'
+    setup()
+    try:
+        loop()
+        destroy()
+    except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+        destroy()
 
+# ex: ts=4 sts=4 sw=4 ai nohls mouse-=a ft=python et:
+# ex: tabstop=4 softtabstop=4 shiftwidth=4 autoindent mouse-=a filetype=python expandtab:
